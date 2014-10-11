37cbe2bbb1b8754a344a407966e90cb27156a9d7
mazeltov7 <ishikawayuki2@gmail.com>
2014-10-11 20:53:11 +0900

    bootstrap＆fontawesome

--- zen/indexproject.php
+++ zen/indexproject.php
@@ -1,1 +1,12 @@
-hoge
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+<head>
+<title>Zen-hack</title>
+<link rel="stylesheet" type="text/css" href="/zenhack/zen/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="/zenhack/zen/css/font-awesome.min.css">
+<script type="text/javascript" href="/js/bootstrap.min.js"></script>
+</head>
+<body>
+<i class="fa fa-line-chart"></i>
+</body>
+</html>
