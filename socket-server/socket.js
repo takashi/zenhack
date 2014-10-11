3ac349bf0266253cf2febfc0f1c3d3c58b7bd7e0
Takashi Nakagawa <tak1240@gmail.com>
2014-10-12 07:16:03 +0900

    connect to remote redis

--- socket-server/socket.js
+++ socket-server/socket.js
@@ -3,29 +3,34 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var redis = require("redis");
 
-var client = redis.createClient();
+var redisClient = redis.createClient(6379, '128.199.136.176');
 
 module.exports = function() {
   http.listen(app.get('port'), function() {
     console.log('listening port for socket.io')
   })
 
-  client.on("error", function (err) {
+  redisClient.on("error", function (err) {
     console.log("Redis Error " + err);
   })
 
-  client.on('connect', function() {
+  redisClient.on('connect', function() {
     console.log('redis connected');
   });
 
+  var cb = function(channel, message) {
+    io.emit('count', message);
+  }
 
-  io.on('connection', function() {
-    console.log('socket.io connected');
-    client.on('message', function(channel, message) {
-      console.log(message);
-      io.emit('count', message);
-    })
+  redisClient.on('disconnect', function(){
+    redisClient.removeListener('message', cb);
   })
 
-  client.subscribe('count');
+
+  io.on('connection', function() {
+    redisClient.removeListener('message', cb);
+    redisClient.on('message', cb);
+  })
+
+  redisClient.subscribe('count');
 }
