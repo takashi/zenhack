f0ac031c679af07a1f5af0fc6c4effdba381731d
mazeltov7 <ishikawayuki2@gmail.com>
2014-10-12 08:47:09 +0900

    hogepiyoooooooooo

--- zen/indexproject.php
+++ zen/indexproject.php
@@ -2,13 +2,40 @@
 <html>
 <head>
 <title>Zen-hack</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link rel="stylesheet" type="text/css" href="/zenhack/zen/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="/zenhack/zen/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="/zenhack/zen/css/style.css">
 <script type="text/javascript" href="/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="page-header">
-  <h1>Zen is zen <small>@kamakura</small></h1>
+
+<div class="wrapper">
+<div class="page-header" style="margin:0 0 20px;">
+  <img src="/zenhack/zen/img/pc/logo.png">
+  <img src="/zenhack/zen/img/pc/search.png">
+  <span style="float:right;font-weight:bold;margin:20px 15px 15px 15px;font-size:16px;">login</span>
+</div>
+
+<div class="content">
+  <h1 class="title">ペットボトル1000本集めて、<br>新入学生にランドセルを配る！</h1>
+  <div style="text-align:center;margin:10px;">
+    <img src="/zenhack/zen/img/takashi.jpg" style="border-radius:60px;width:60px;">
+    <p><span>projected by<br><strong><a href="">鎌倉市立今泉小学校</a></strong></span></p>
+    <div style="position:relative;display:block;">
+      <img src="/zenhack/zen/img/randosel.jpg" width="840px" style="border-radius:10px;">
+      <p style="position:absolute;top:340px;left:480px;font-size:62px;color:white;font-weidht:bold;"><strong>180</strong> / 1000</p>
+    </div>
+  </div>
+  <div class="content-left">
+    <p>
+      <img src="/zenhack/zen/img/pc/about.png">
+    </p>
+  </div>
+  <div style="text-align:center float:right; display:inline-block;">hoge</div>
+</div>
+
+  
 </div>
 </body>
 </html>
