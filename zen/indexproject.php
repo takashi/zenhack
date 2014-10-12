a317a8932cb0ab5403c5e7270d2dd0c1da9ae7d5
mazeltov7 <ishikawayuki2@gmail.com>
2014-10-12 10:42:08 +0900

    左コンテンツ、サポーター

--- zen/indexproject.php
+++ zen/indexproject.php
@@ -10,32 +10,89 @@
 </head>
 <body>
 
+  <div class="page-header" style="margin:0 0 20px;background-color:white;width:100%;padding-bottom:0;">
+    <img src="/zenhack/zen/img/pc/logo.png" style="padding-left:10px;">
+    <img src="/zenhack/zen/img/pc/search.png">
+    <span style="float:right;font-weight:bold;margin:20px 15px 15px 15px;font-size:16px;">login</span>
+  </div>
+
 <div class="wrapper">
-<div class="page-header" style="margin:0 0 20px;">
-  <img src="/zenhack/zen/img/pc/logo.png">
-  <img src="/zenhack/zen/img/pc/search.png">
-  <span style="float:right;font-weight:bold;margin:20px 15px 15px 15px;font-size:16px;">login</span>
-</div>
-
-<div class="content">
-  <h1 class="title">ペットボトル1000本集めて、<br>新入学生にランドセルを配る！</h1>
-  <div style="text-align:center;margin:10px;">
-    <img src="/zenhack/zen/img/takashi.jpg" style="border-radius:60px;width:60px;">
-    <p><span>projected by<br><strong><a href="">鎌倉市立今泉小学校</a></strong></span></p>
-    <div style="position:relative;display:block;">
-      <img src="/zenhack/zen/img/randosel.jpg" width="840px" style="border-radius:10px;">
-      <p style="position:absolute;top:340px;left:480px;font-size:62px;color:white;font-weidht:bold;"><strong>180</strong> / 1000</p>
+  <div class="content">
+    <h1 class="title">ペットボトル1000本集めて、<br>新入学生にランドセルを配る！</h1>
+    <div style="text-align:center;margin:10px;">
+      <img src="/zenhack/zen/img/takashi.jpg" style="border-radius:60px;width:60px;">
+      <p><span>projected by<br><strong><a href="">鎌倉市立今泉小学校</a></strong></span></p>
+      <div style="position:relative;display:block;">
+        <img src="/zenhack/zen/img/randosel.jpg" width="840px" style="border-radius:10px;">
+        <p style="position:absolute;top:340px;left:480px;font-size:62px;color:white;font-weidht:bold;"><strong>180</strong> / 1000</p>
+      </div>
     </div>
-  </div>
-  <div class="content-left">
-    <p>
-      <img src="/zenhack/zen/img/pc/about.png">
-    </p>
-  </div>
-  <div style="text-align:center float:right; display:inline-block;">hoge</div>
-</div>
+    <section class="content-left">
+      <p>
+        <img src="/zenhack/zen/img/pc/about.png">
+        <span style="font-weight:bold;font-size:18px;margin-left:5px;vertical-align:middle;">このプロジェクトについて</span>
+      </p>
+      <div style="background-color:white;padding:15px 20px;border-radius:10px;">
+        <h3 style="margin-top:10px;margin-bottom:20px;">ランドセルを配りたい</h3>
+        <p style="line-height:1.8;">
+          私達は、鎌倉市立今泉小学校PTAです。<br>
+          毎年、数十名の新入生が入ってきますが、その新入生に向けて、ランドセルをこのプロジェクトで贈りたいと思っています。
+        </p>
+        <p style="line-height:1.8;">
+          鎌倉私立今泉小学校の学区の家庭で出たペットボトルを分別して、リサイクルしてランドセルを購入します。
+        </p>
+        <p style="margin:20px 0;">
+          <img src="/zenhack/zen/img/PTA.jpg">
+        </p>
+        <p style="line-height:1.8;">
+          地域のみんなで目標達成したいと思っています。<br>
+          応援をどうぞよろしくお願いします。
+        </p>
+      </div>
+    </section>
+    <section class="sidebar">
+      <p>
+        <img src="/zenhack/zen/img/pc/people.png">
+        <span style="font-weight:bold;font-size:18px;vertical-align:middle;">サポーター</span>
+        <span style="margin-left:100px;font-weight:bold;font-size:16px;vertical-align:middle;">1200</span>
+      </p>
+      <div style="padding:15px 0;margin-right:65px;">
+        <img src="/zenhack/zen/img/ito.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin2.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin3.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin4.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin5.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin7.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin8.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin9.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/takashi.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/ito.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin2.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin3.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin4.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin5.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin7.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin8.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin9.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/takashi.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/ito.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin2.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin3.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin4.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin5.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin7.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin8.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/shasin9.jpg" width="50px;" style="border:1px solid #B9BABD;">
+        <img src="/zenhack/zen/img/takashi.jpg" width="50px;" style="border:1px solid #B9BABD;">
 
-  
+
+      </div>
+    </section>
+  </div>
+
 </div>
 </body>
 </html>
